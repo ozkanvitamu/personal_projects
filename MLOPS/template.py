46f6e84dbe4dec4231b2ed7f2c8fb1ce65427fa7
ozkanvitamu <ozkan@vitamu.com>
2024-03-15 11:56:19 +0300

    files updated structure & Logging screen

--- MLOPS/template.py
+++ MLOPS/template.py
@@ -10,16 +10,25 @@ list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
     f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
+    "schema.yaml",
+    "Dockerfile",
+    "app.py",
+    "main.py",
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
+    "templates/index.html",
+    "test.py"
 ]
 
 for filepath in list_of_files:
@@ -37,4 +46,3 @@ for filepath in list_of_files:
 
     else:
         logging.info(f"File already exists: {filename}")
-
